ccaeb4c1319b69f9980bc9b1ba25a026e69edc98
Noor Fatima <154274914+NoorFatimaAfzal@users.noreply.github.com>
2024-02-11 16:58:04 +0500

    pattern task

--- p1.py
+++ p1.py
@@ -8,4 +8,11 @@ for i in range(5) :
 for num in range(5):
     for i in range(num):
         print(num, end="")
-    print()    
+    print()
+
+b=0 
+for i in range(5, 0, -1):
+    b+=1
+    for num in range(1, i+1):
+        print(b,end="")
+    print()
